dfe5e7d13ff30b16e39de84166f0f4d3da48179c
cfy66623 <cfy66623@example.com>
2026-05-04 13:26:43 +0800

    fix api base url for production

--- mold-ui/lib/api.ts
+++ mold-ui/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://127.0.0.1:8000";
+const API = "https://moldai.onrender.com";
 
 export const predictAPI = (file: File) => {
     const formData = new FormData();
